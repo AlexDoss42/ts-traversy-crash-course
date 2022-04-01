e783b604ac1822615f0c5c6c351ef5fc241e351f
AlexDoss42 <alex.doss42@gmail.com>
2022-04-01 16:50:06 -0600

    does some basic examples of data types

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,40 @@
 "use strict";
+// basic types
 let id = 5;
+let company = 'Doss Capital';
+let isPublished = true;
+let x = 'Hello';
+let age;
+x = true;
+age = 30;
+let ids = [1, 2, 3, 4, 5];
+let arr = [1, 2, 'hello world'];
+// Tuple
+let person = [1, 'bob', true];
+//  Tuple Array 
+let employee;
+employee = [
+    [1, 'jane'],
+    [2, 'jill'],
+    [3, 'jack'],
+    [4, 'john'],
+];
+let ssid = 22;
+ssid = '22';
+// Enum
+var Dircetion1;
+(function (Dircetion1) {
+    Dircetion1[Dircetion1["Up"] = 1] = "Up";
+    Dircetion1[Dircetion1["Down"] = 2] = "Down";
+    Dircetion1[Dircetion1["Left"] = 3] = "Left";
+    Dircetion1[Dircetion1["Right"] = 4] = "Right";
+})(Dircetion1 || (Dircetion1 = {}));
+const user = {
+    id: 1,
+    name: 'Jacob'
+};
+// Type Assertion
+let cid = 1;
+// let customerId = <number>cid
+let customerId = cid;
 console.log(id);
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,59 @@
+// basic types
+
 let id: number = 5;
+let company: string = 'Doss Capital';
+let isPublished: boolean = true;
+let x: any = 'Hello';
+let age: number
+
+x = true
+age = 30
+
+let ids: number[] = [1,2,3,4,5]
+let arr: any[] = [1,2,'hello world']
+
+// Tuple
+let person: [number, string, boolean] = [1, 'bob', true]
+
+//  Tuple Array 
+
+let employee: [number, string][]
+
+employee = [
+    [1, 'jane'],
+    [2, 'jill'],
+    [3, 'jack'],
+    [4, 'john'],
+]
+
+let ssid: string | number = 22
+ssid = '22'
+
+// Enum
+
+enum Dircetion1 {
+    Up = 1,
+    Down,
+    Left,
+    Right
+}
+
+// objects
+
+type User = {
+    id: number,
+    name: string
+}
+
+const user: User = {
+    id: 1,
+    name: 'Jacob'
+}
+
+// Type Assertion
+
+let cid: any = 1
+// let customerId = <number>cid
+let customerId = cid as number
 
 console.log(id)
